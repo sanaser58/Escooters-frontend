d69b92fce0ef7730a055edd075b44386bb372bcf
sayednaser58 <sanaser9876@gmail.com>
2020-12-02 22:52:20 -0500

    implement the react router and everything working good

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductView from './screens/ProductView'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductView} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
