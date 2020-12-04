f6ee4ed102025e5bb2b593c3d779f7caa0017db5
sayednaser58 <sanaser9876@gmail.com>
2020-12-04 17:17:40 -0500

    finishing up implementing bthe redux in product view page,

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux' /// bringing necessary stuff from redux
 import thunk from 'redux-thunk' /// middleware
 import { composeWithDevTools } from 'redux-devtools-extension' /// to use redux devtools
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer, productDetailsReducer} from './reducers/productReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 
 const initialState = {}
